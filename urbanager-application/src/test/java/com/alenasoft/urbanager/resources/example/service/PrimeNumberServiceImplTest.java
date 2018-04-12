45311b128fb429bc95a769244e497e73c9360cc6
Luis Lopez <llopez@cloudbit.com.bo>
2018-04-11 22:33:06 -0400

    Added second test: testIsPrimeNumberIfGivenNumberEqualsZeroReturnFalse Test failed

--- urbanager-application/src/test/java/com/alenasoft/urbanager/resources/example/service/PrimeNumberServiceImplTest.java
+++ urbanager-application/src/test/java/com/alenasoft/urbanager/resources/example/service/PrimeNumberServiceImplTest.java
@@ -27,4 +27,11 @@ public class PrimeNumberServiceImplTest {
         Boolean result = this.primeService.isPrimeNumber(-9);
         assertEquals(expectedValue, result);
     }
+
+    @Test
+    public void testIsPrimeNumberIfGivenNumberEqualsZeroReturnFalse() {
+        Boolean expectedValue = false;
+        Boolean result = this.primeService.isPrimeNumber(0);
+        assertEquals(expectedValue, result);
+    }
 }
