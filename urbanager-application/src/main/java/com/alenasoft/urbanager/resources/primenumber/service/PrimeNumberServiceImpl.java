c15ba994e9c780eb7646aba54272e1a259d01276
Luis Lopez <llopez@cloudbit.com.bo>
2018-04-11 22:44:45 -0400

    Refactorized method isPrimeNumber() for test: testIsPrimeNumberIfGivenNumberEqualsTwoReturnFalse Test passed

--- urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primenumber/service/PrimeNumberServiceImpl.java
+++ urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primenumber/service/PrimeNumberServiceImpl.java
@@ -4,7 +4,7 @@ public class PrimeNumberServiceImpl implements PrimeNumberService{
 
     @Override
     public Boolean isPrimeNumber(int value) {
-        if (value <= 1){
+        if (value <= 2){
             return false;
         }else {
             return true;
