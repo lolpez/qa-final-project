cb5a5b5378ba03d6ea5d8bbb793cacfa6aa2f2c9
Luis Lopez <llopez@cloudbit.com.bo>
2018-04-11 23:03:33 -0400

    Refactorized method isPrimeNumber()

--- urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primenumber/service/PrimeNumberServiceImpl.java
+++ urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primenumber/service/PrimeNumberServiceImpl.java
@@ -1,18 +1,13 @@
 package com.alenasoft.urbanager.resources.primenumber.service;
 
-public class PrimeNumberServiceImpl implements PrimeNumberService{
+public class PrimeNumberServiceImpl implements PrimeNumberService {
 
     @Override
     public Boolean isPrimeNumber(int value) {
-        if (value <= 2){
-            return false;
-        }else {
-            for (int i = 2; i < value; i++){
-                if(value % i == 0){
-                    return  false;
-                }
-            }
-            return true;
+        if (value <= 2) return false;
+        for (int i = 2; i < value; i++) {
+            if (value % i == 0) return false;
         }
+        return true;
     }
 }
